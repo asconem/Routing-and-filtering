70c3b3b3889d05c389d3240bac935cdf59478f88
Matthew Ascone <asconem@gmail.com>
2020-07-16 16:47:41 -0400

    Add SimpleFilter class

--- gateway/src/main/java/com/example/routingandfilteringgateway/RoutingAndFilteringGatewayApplication.java
+++ gateway/src/main/java/com/example/routingandfilteringgateway/RoutingAndFilteringGatewayApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import com.example.routingandfilteringgateway.filters.pre.SimpleFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
