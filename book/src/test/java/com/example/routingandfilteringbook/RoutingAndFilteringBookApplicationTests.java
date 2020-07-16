956de61981ccbb5174c0ad1f524bf8b42b574994
Matthew Ascone <asconem@gmail.com>
2020-07-16 16:56:13 -0400

    Add availableTest in book

--- book/src/test/java/com/example/routingandfilteringbook/RoutingAndFilteringBookApplicationTests.java
+++ book/src/test/java/com/example/routingandfilteringbook/RoutingAndFilteringBookApplicationTests.java
@@ -2,13 +2,21 @@ package com.example.routingandfilteringbook;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RoutingAndFilteringBookApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private TestRestTemplate rest;
 
+	@Test
+	public void availableTest() {
+		String resp = rest.getForObject("/available", String.class);
+		assertThat(resp).isEqualTo("Spring in Action");
+	}
 }
